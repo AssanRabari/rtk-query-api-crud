4f0f4a938385264383974eeb885394b46e8c1ddc
AssanRabari <assandesai@gmail.com>
2024-06-26 08:46:03 +0530

    created store and configured reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,14 @@
 import { confiqureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { postApi } from "../services/post";
+
+export const store = confiqureStore({
+    reducer: {
+        [postApi.reducerPath]: postApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => {
+        getDefaultMiddleware().concat(postApi.middleware)
+    }
+})
+
+setupListeners(store.dispatch)
